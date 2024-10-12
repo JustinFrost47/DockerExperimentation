71bf02df823b98a94de52cddeb7bee69613121f8
JustinFrost47 <rinojonathanarul@gmail.com>
2024-10-12 20:07:48 +0530

    phone routes

--- backend/src/routes/phoneRoutes.ts
+++ backend/src/routes/phoneRoutes.ts
@@ -1,9 +1,15 @@
 // routes/phoneRoutes.ts
 import { Router } from 'express';
 import sampleController from '../controllers/sampleController';
+import { mobileController } from '../controllers/mobileController';
 
 const router = Router();
 
-router.get('/', sampleController.greet);
+router.get('/', mobileController.getAllMobiles)
+router.get('/:id', mobileController.getMobileByID)
+router.post('/', mobileController.addMobileEntry);
+router.patch('/increaseStock/:id', mobileController.increaseStock)
+router.patch('/decreaseStock/:id', mobileController.decreaseStock)
+router.delete('/:id', mobileController.deleteMobileByID)
 
 export default router ;
